3d61340efae43a9a6eae22793398b270653815cd
KBModib <kmodibedi@outlook.com>
2022-02-23 11:57:34 +0200

    first commit,sort of

---
+++ contact.js
@@ -0,0 +1,16 @@
+script>
+  
+   function myClickFunction() {
+          let phone, email, LinkedIn_Profile,
+          twitter;
+          phone = '(+27)64 867 9973';
+          email = 'kmodibedi@outlook.com';
+          LinkedIn_Profile = 'Linked';
+          twitter = '@kbmodi07';
+          document.getElementById(contact).innerHTML = phone + email + LinkedIn_Profile + twitter;
+          document.getElementById(q&comms).innerHTML = 'Questions and Comments'
+
+   }
+    
+   
+</script>
